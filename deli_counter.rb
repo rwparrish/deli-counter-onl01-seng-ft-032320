242c997623e2035b2b4ef49e19812461d2cf21f3
Ryan Parrish <rino3717@gmail.com>
2020-03-25 22:09:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,11 @@ def line(name)
   
   if arr.length > 0
     
-    puts "The line is currently empty."
+    arr.each.with_index.map { |name, i|
     
   else
     
-    
+    puts "The line is currently empty."
     
   end
 
