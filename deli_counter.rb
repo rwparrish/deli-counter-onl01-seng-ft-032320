dfe76c1eff881a619b22714e8ae846a00f730fb8
Ryan Parrish <rino3717@gmail.com>
2020-03-25 23:51:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,13 +12,12 @@ end
  
  
 def take_a_number(katz_deli, name)
-  
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  
 end
 
 def now_serving(katz_deli)
+  puts "Currently serving #{katz_deli[0]}."
   
   
   
