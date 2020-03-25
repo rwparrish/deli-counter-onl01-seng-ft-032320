bfe622ea1ddcb0b39ce05d0b3fe350020108b88b
Ryan Parrish <rino3717@gmail.com>
2020-03-25 23:45:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,6 @@ end
 def take_a_number(katz_deli, name)
   
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli[-1]}."
+  puts "Welcome, #{name}. You are number #{katz_deli.length}."
   
 end
