a678a8669efb2dea9dc396091be731d7a0286db7
Ryan Parrish <rino3717@gmail.com>
2020-03-25 22:58:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,16 @@
 def line(katz_deli)
   
-  katz_deli = []
-  
   if katz_deli.length > 0
+    
+    serving = "The line is currently: "
+  
+    katz_deli.each.with_index do | name, i | 
+      
+    serving += "#{index += 1}. #{name} "
+    
+  end
+  
+  puts serving 
+    
+  puts "The line is currently empty"
  
