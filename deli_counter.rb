d6ab16c53eaaa05a655dff985ba0f485377d1da9
Ryan Parrish <rino3717@gmail.com>
2020-03-25 22:42:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,4 @@
 def line(katz_deli)
   
-  def line(katz_deli)
-    
-  if katz_deli.length > 0
-    
-    line_number = 1
-    
-    katz_deli.each do |name|
-      
-    return "The line is currently: #{}"
-    
-    line_number +=1
-
-  else
   
-    "The line is currently empty."
-    
-  end
-  
-end
+ 
