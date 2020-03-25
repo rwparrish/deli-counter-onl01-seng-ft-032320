6677247bed432c96cac796c22af9d74f934b311f
Ryan Parrish <rino3717@gmail.com>
2020-03-25 22:59:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,11 @@ def line(katz_deli)
   
   if katz_deli.length > 0
     
-    serving = "The line is currently: "
+    serving = "The line is currently:"
   
     katz_deli.each.with_index do | name, i | 
       
-    serving += "#{index += 1}. #{name} "
+    serving += "#{index += 1}. #{name}"
     
     end
   
