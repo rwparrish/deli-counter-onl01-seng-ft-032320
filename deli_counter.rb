5039a6de43c070b277de6e545581a38915dd3d04
Ryan Parrish <rino3717@gmail.com>
2020-03-25 23:13:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,20 +3,16 @@ def line(katz_deli)
      serving = "The line is currently:"
       katz_deli.each_with_index do |name, index| 
       serving += " #{index += 1}. #{name}"
-    
     end
-  
     puts serving 
-  
   else
-    
     puts "The line is currently empty."
-  
   end
-
 end
  
  
 def take_a_number(katz_deli, name)
   
+  katz_deli << name
   
+end
